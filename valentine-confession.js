968cc939aba56a5c9cd67aa1485913ae7508a683
sanjog11 <sanjoglwagun5@gmail.com>
2025-05-10 19:13:47 +0545

    Happy Anniversary

---
+++ valentine-confession.js
@@ -0,0 +1,45 @@
+// Existing checkbox change logic
+$("#messageState").on("change", (x) => {
+    $(".message").removeClass("openNor").removeClass("closeNor");
+    if ($("#messageState").is(":checked")) {
+        $(".message").removeClass("closed").removeClass("no-anim").addClass("openNor");
+        $(".heart").removeClass("closeHer").removeClass("openedHer").addClass("openHer");
+        $(".container").stop().animate({ "backgroundColor": "#f48fb1" }, 2000);
+        console.log("Abrindo");
+    } else {
+        $(".message").removeClass("no-anim").addClass("closeNor");
+        $(".heart").removeClass("openHer").removeClass("openedHer").addClass("closeHer");
+        $(".container").stop().animate({ "backgroundColor": "#fce4ec" }, 2000);
+        console.log("fechando");
+
+        // Hide the button when closing
+        $("#finalButton").fadeOut();
+    }
+});
+
+// After message animation ends
+$(".message").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function (e) {
+    console.log("Animation End");
+
+    if ($(".message").hasClass("closeNor"))
+        $(".message").addClass("closed");
+
+    $(".message").removeClass("openNor").removeClass("closeNor").addClass("no-anim");
+
+    // Show the button after opening animation
+    if ($("#messageState").is(":checked")) {
+        $("#finalButton").fadeIn(1000); // Show with a fade-in
+    }
+});
+
+// After heart animation ends
+$(".heart").on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function (e) {
+    console.log("Animation End");
+
+    if (!$(".heart").hasClass("closeHer"))
+        $(".heart").addClass("openedHer").addClass("beating");
+    else
+        $(".heart").addClass("no-anim").removeClass("beating");
+
+    $(".heart").removeClass("openHer").removeClass("closeHer");
+});
